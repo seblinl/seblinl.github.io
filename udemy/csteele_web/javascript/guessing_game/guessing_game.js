82de927c06eb16efbe4110fbdc1b4064a878b57c
Sebastian <sebastianlindbergl@gmail.com>
2022-07-04 18:57:21 +0200

    sorting and adjusting

---
+++ udemy/csteele_web/javascript/guessing_game/guessing_game.js
@@ -0,0 +1,29 @@
+let maximum = parseInt(prompt("Enter a maximum number"));
+
+while (!maximum) {
+  maximum = parseInt(prompt("Not valid, Try again"));
+}
+
+const random = Math.floor(Math.random() * maximum) + 1;
+console.log(`The number is between 1 and ${maximum}`);
+
+let guess = prompt("Make a guess");
+let attempts = 1;
+
+while (parseInt(guess) !== random && guess.toLowerCase() !== "q") {
+  console.log(`Attempt: ${attempts}, Guess: ${guess}`);
+  attempts++;
+  if (parseInt(guess) > random) {
+    guess = prompt(guess + " is too high, try again");
+  } else {
+    guess = prompt(guess + " is too low, try again");
+  }
+}
+
+if (guess.toLowerCase() === "q") {
+  console.log(
+    `Quitting the game after ${attempts} attempt(s), the number was ${random}`
+  );
+} else {
+  console.log(`You guessed ${random} in ${attempts} attempt(s)`);
+}
---
+++ udemy/csteele_web/javascript/todo-list/todo.js
@@ -0,0 +1,34 @@
+const todo = [];
+
+let action = prompt("Input a command").toLowerCase();
+
+while (action !== "quit" && action !== "q") {
+  switch (action.toLowerCase()) {
+    case "new":
+      const newEntry = prompt("Add a new todo entry");
+      todo.push(newEntry);
+      console.log(`${newEntry} was added to the list`);
+      break;
+    case "list":
+      console.log("***");
+      for (let entry of todo) {
+        console.log(`${todo.indexOf(entry)}: ${entry}`);
+      }
+      console.log("***");
+      break;
+    case "delete":
+      const removeEntry = parseInt(
+        prompt("Input an entrys index to remove it from the list")
+      );
+      if (!Number.isNaN(removeEntry)) {
+        console.log(`${todo.splice(removeEntry, 1)} was removed from the list`);
+      } else {
+        console.log("Invalid index");
+      }
+      break;
+  }
+
+  action = prompt("New command");
+}
+
+console.log("You quit the app");
